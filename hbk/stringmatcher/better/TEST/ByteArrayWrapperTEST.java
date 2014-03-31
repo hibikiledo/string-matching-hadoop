f982f27a0daeb3d23ce12d190137d2d89f23879b
nutsgmuic@gmail.com <nutsgmuic@gmail.com@615efaa8-ff02-4593-3b6d-8bee57df7ccf>
2014-03-31 12:26:56 +0000

    Test duplicate byte array and add more bug.

--- hbk/stringmatcher/better/TEST/ByteArrayWrapperTEST.java
+++ hbk/stringmatcher/better/TEST/ByteArrayWrapperTEST.java
@@ -2,18 +2,27 @@ package hbk.stringmatcher.better.TEST;
 
 import hbk.stringmatcher.better.BytesArrayWrapper;
 
+import java.util.Iterator;
+
 public class ByteArrayWrapperTEST {
 
     public static void main(String[] args) {
 
         BytesArrayWrapper b = new BytesArrayWrapper();
         byte[] bIn1 = new byte[]{'a','b'};
-        byte[] bIn2 = new byte[]{'c','d'};
+        byte[] bIn2 = new byte[]{'a','b'};
         byte[] bIn3 = new byte[]{'a','b','c'};
+        byte[] bIn4 = new byte[]{'c','d','e'};
 
         b.add(bIn1);
         b.add(bIn2);
         b.add(bIn3);
+        b.add(bIn4);
+
+        Iterator<byte[]> i = b.getIterator();
+        while(i.hasNext()) {
+            System.out.println(new String(i.next()));
+        }
 
         System.out.println(b.size());
         System.out.println(b.getMaxSize());

94deb4b71766ac256305e85422c5823f49b301bf
nutsgmuic@gmail.com <nutsgmuic@gmail.com@615efaa8-ff02-4593-3b6d-8bee57df7ccf>
2014-03-31 12:31:50 +0000

    Clean up the code, and remove unused variables.

--- hbk/stringmatcher/better/SuperLongReducer.java
+++ hbk/stringmatcher/better/SuperLongReducer.java
@@ -12,11 +12,7 @@ import java.util.Iterator;
 
 public class SuperLongReducer extends MapReduceBase implements Reducer<Text, LongWritable, Text, Text> {
 
-    // Char in java is 2 byte, 16 bit
-    // Allow maximum of 2M / value pair = 1048576 in string length
-
     private Text outValue = new Text();
-    private static final int MAX_VALUE_LIMIT = 1048576;
 
     @Override
     public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<Text, Text> output, Reporter reporter)
@@ -26,7 +22,6 @@ public class SuperLongReducer extends MapReduceBase implements Reducer<Text, Lon
         boolean isFirstElementPassed = false;
 
         while( values.hasNext() ) {
-
             if(!isFirstElementPassed) {
                 sb.append(values.next().get());
                 isFirstElementPassed = true;
@@ -34,24 +29,10 @@ public class SuperLongReducer extends MapReduceBase implements Reducer<Text, Lon
             else {
                 sb.append(',').append(values.next().get());
             }
-
-            // if sb.length more than limit , collect once
-            if(sb.length() > MAX_VALUE_LIMIT) {
-                // Debug
-                System.out.println("Reduce Output: " + key + "<>" + sb.toString());
-
-                outValue.set(sb.toString());
-                output.collect(key, outValue);
-                sb = new StringBuilder();
-            }
         }
-
         outValue.set(sb.toString());
-        // Debug
-        System.out.println("Reduce Output: " + key + "<>" + sb.toString());
-        // Key is string, value are offset and fileName
-        output.collect(key, outValue);
 
+        output.collect(key, outValue);
     }
 }
 

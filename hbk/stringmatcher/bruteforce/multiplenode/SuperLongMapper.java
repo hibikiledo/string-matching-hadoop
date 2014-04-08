c83fe36ef726f61c0e631186b12a2285c7f4e75a
nutsgmuic@gmail.com <nutsgmuic@gmail.com@615efaa8-ff02-4593-3b6d-8bee57df7ccf>
2014-04-08 02:01:03 +0000

    Keep query in memory instead of reading from file.

--- hbk/stringmatcher/bruteforce/multiplenode/SuperLongMapper.java
+++ hbk/stringmatcher/bruteforce/multiplenode/SuperLongMapper.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapred.*;
 
 import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
 
 public class SuperLongMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, LongWritable> {
 
@@ -14,43 +15,46 @@ public class SuperLongMapper extends MapReduceBase implements Mapper<LongWritabl
     private Text keyOut = new Text();
     private LongWritable valueOut = new LongWritable();
     private String currentFile;
+    private ArrayList<String> query = new ArrayList<String>();
 
     @Override
     public void configure(JobConf job) {
         try {
             URI[] files = DistributedCache.getCacheFiles(job);
             stringListFile = new File(files[0].getPath());
+            initQuery();
         } catch (IOException e) {
             System.err.println( e );
+        }
+    }
+
+    private void initQuery() throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(stringListFile)));
+        String line;
+        while((line=reader.readLine())!=null) {
+            query.add(line);
         }
     }
 
     @Override
     public void map(LongWritable key, Text value, OutputCollector<Text, LongWritable> output, Reporter reporter) throws IOException {
 
-        // Debug
-        // System.out.println("String" + ":" + value);
-
         // Get name
         currentFile = ((FileSplit)reporter.getInputSplit()).getPath().getName();
+        String v = value.toString();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(stringListFile)));
-        String line;
-        while((line = reader.readLine()) != null) {
-            if(line.equals(value.toString())) {
+        for(String q : query) {
+
+            if( q.equals( v.substring(0, Math.min(v.length(), q.length()))) ) {
                 // Key of map to be the filename and string
-                keyOut.set(currentFile+','+line);
+                keyOut.set(currentFile+','+q);
 
                 // Value is offset
                 valueOut.set( key.get() );
 
                 output.collect(keyOut, valueOut);
-                System.out.println("Map Output: " + keyOut + "<>" +  valueOut.get());
+                //System.out.println("Map Output: " + keyOut + "<>" +  valueOut.get());
             }
         }
-
-        reader.close();
-
     }
 }
-

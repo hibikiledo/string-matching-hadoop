33ab504565ebcce239e0010ddcae13405aa54552
nutsgmuic@gmail.com <nutsgmuic@gmail.com@615efaa8-ff02-4593-3b6d-8bee57df7ccf>
2014-04-14 06:55:04 +0000

    Edit reducer to simply collect key and value to OutputFormat.

--- trunk/hbk/stringmatcher/better/SuperLongReducer.java
+++ trunk/hbk/stringmatcher/better/SuperLongReducer.java
@@ -10,21 +10,13 @@ import org.apache.hadoop.mapred.Reporter;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class SuperLongReducer extends MapReduceBase implements Reducer<Text, LongWritable, Text, SuperLongValueWrapper> {
-
-    private SuperLongValueWrapper valueOut = new SuperLongValueWrapper();
+public class SuperLongReducer extends MapReduceBase implements Reducer<Text, LongWritable, Text, LongWritable> {
 
     @Override
-    public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<Text, SuperLongValueWrapper> output, Reporter reporter)
+    public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<Text, LongWritable> output, Reporter reporter)
             throws IOException {
-
-        valueOut = new SuperLongValueWrapper();
-
         while( values.hasNext() ) {
-            valueOut.appendValue(values.next().get());
+            output.collect(key, values.next());
         }
-
-        output.collect(key, valueOut);
     }
 }
-

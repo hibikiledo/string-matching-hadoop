0041f3beb2bf433887478b33307d383019c2f3d1
nutsgmuic@gmail.com <nutsgmuic@gmail.com@615efaa8-ff02-4593-3b6d-8bee57df7ccf>
2014-04-14 06:54:15 +0000

    Improve reducer performance.

--- trunk/hbk/stringmatcher/better/SuperLongOutputFormat.java
+++ trunk/hbk/stringmatcher/better/SuperLongOutputFormat.java
@@ -3,6 +3,7 @@ package hbk.stringmatcher.better;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
@@ -10,12 +11,15 @@ import org.apache.hadoop.util.Progressable;
 
 
 import java.io.*;
+import java.nio.ByteBuffer;
 
 public class SuperLongOutputFormat<K,V> extends FileOutputFormat<K,V> {
 
     protected static class LineRecordWriter<K, V> implements RecordWriter<K, V> {
         private static final String utf8 = "UTF-8";
         private static final byte[] newline;
+        private static String latestKey=null;
+        private static String v;
         static {
             try {
                 newline = "\n".getBytes(utf8);
@@ -47,38 +51,9 @@ public class SuperLongOutputFormat<K,V> extends FileOutputFormat<K,V> {
                 out.write(to.getBytes(), 0, to.getLength());
             }
 
-            if( o instanceof SuperLongValueWrapper) {
-
-                SuperLongValueWrapper v = (SuperLongValueWrapper) o;
-
-                int start = v.getFirstChunkIndex();
-                int end = v.getLastChunkIndex();
-
-                if(!v.isExceed()) { // no need to read from chunks
-                    String valueStr = v.getValue();
-                    for(int i=0; i<valueStr.length(); i++) {
-                        out.write( valueStr.charAt(i) );
-                    }
-                } else { // there is/are chunk(s)
-
-                    System.out.println("Doing from " + start + ">" + end);
-
-                    // write those in file as well
-                    for(int i=start; i<=end; i++) {
-                        FileInputStream is = new FileInputStream(String.valueOf(i));
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                        String chunkData = reader.readLine();
-                        for(int j=0; j<chunkData.length(); j++) {
-                            out.write(chunkData.charAt(j));
-                        }
-                    }
-
-                    // also get some portion of left in string builder as well
-                    String valueStr = v.getValue();
-                    for(int i=0; i<valueStr.length(); i++) {
-                        out.write( valueStr.charAt(i) );
-                    }
-                }
+            if( o instanceof LongWritable) {
+                v = String.valueOf(((LongWritable)o).get());
+                out.write(v.getBytes(), 0, v.length());
             }
         }
 
@@ -91,15 +66,28 @@ public class SuperLongOutputFormat<K,V> extends FileOutputFormat<K,V> {
                 return;
             }
             if (!nullKey) {
-                writeObject(key);
+                // handle first k,v
+                if(latestKey==null) {
+                    writeObject(key);
+                    out.write(keyValueSeparator);
+                    writeObject(value);
+                    latestKey = key.toString();
+                    return;
+                }
+                if(!key.toString().equals(latestKey)) {
+                    out.write(newline);
+                    writeObject(key);
+                    latestKey = key.toString();
+                }
             }
             if (!(nullKey || nullValue)) {
-                out.write(keyValueSeparator);
+                if(!key.toString().equals(latestKey))
+                    out.write(keyValueSeparator);
             }
             if (!nullValue) {
+                out.write(keyValueSeparator);
                 writeObject(value);
             }
-            out.write(newline);
         }
 
         public synchronized void close(Reporter reporter) throws IOException {
@@ -117,5 +105,4 @@ public class SuperLongOutputFormat<K,V> extends FileOutputFormat<K,V> {
 
         return new LineRecordWriter<K, V>(fileOut, keyValueSeparator);
     }
-
 }
